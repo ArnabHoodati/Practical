e6adaa31120ff9ef57d324e0e3e8c16bb3dde1da
ArnabHoodati <107671204+ArnabHoodati@users.noreply.github.com>
2022-06-22 13:55:48 +0530

    Add files via upload

---
+++ Div.java
@@ -0,0 +1,10 @@
+class Div{
+    public static void main(String args[]){
+  
+    int a=100;    
+    int b=10;    
+    int c=(a/b);
+    System.out.println("division:" +c);
+  
+}  
+}  
---
+++ Sub.java
@@ -0,0 +1,10 @@
+class Sub{
+    public static void main(String args[]){
+  
+    int a=100;    
+    int b=10;    
+    int c=(a-b);
+    System.out.println("Substraction:" +c);
+  
+}  
+}  
