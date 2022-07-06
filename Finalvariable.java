37a1a869a72db5d98a6fd2b2279393c96d194503
ArnabHoodati <107671204+ArnabHoodati@users.noreply.github.com>
2022-07-07 01:18:35 +0530

    Add files via upload

---
+++ Finalvariable.java
@@ -0,0 +1,15 @@
+/*
+@author: Arnab Kumar Hoodati
+*/
+
+
+public class Finalvariable {
+	final int carspeed=60;
+	void run() {
+		//carspeed=90;
+		}
+public static void main (String[] args) {
+Finalvariable a=new Finalvariable();
+	a.run();
+}
+}
