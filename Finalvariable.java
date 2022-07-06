b7cd997b8a486edb59b2a6ee560d2fb985c3fcc6
ArnabHoodati <107671204+ArnabHoodati@users.noreply.github.com>
2022-07-07 01:19:45 +0530

    Update Finalvariable.java

--- Finalvariable.java
+++ Finalvariable.java
@@ -6,8 +6,8 @@
 public class Finalvariable {
 	final int carspeed=60;
 	void run() {
-		//carspeed=90;
-		}
+		carspeed=90;
+}
 public static void main (String[] args) {
 Finalvariable a=new Finalvariable();
 	a.run();
